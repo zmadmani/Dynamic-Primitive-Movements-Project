ef19ec0f43ebc0da3e88b9aabaeaad7694787857
zmadmani <zain.admani@gmail.com>
2016-09-07 02:37:53 -0500

    Initial Code push

---
+++ DMP.py
@@ -0,0 +1,13 @@
+import math
+
+ALPHA = -1*math.log(0.01) #alpha that leads to 99% convergence
+
+def DMPLearning(trajectory, K, D):
+    # Make sure D = 2*sqrt(K)
+    pass
+
+def DMPPlanning(parameters, startPos, startVel, endPos, t, dt):
+    pass
+
+def approximate_function():
+    pass
